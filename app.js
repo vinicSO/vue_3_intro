0008abea0b76f2e506af8493060a2bba9a8aa777
Vinícius S Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-03-09 16:09:50 -0300

    using bind

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const app = {
     data () {
         return {
             name: 'Carlos',
-            lastName: 'Ferreira'
+            lastName: 'Ferreira',
+            product: {
+                title: 'Filme 01',
+                description: 'Filme muito legal',
+                image: './assets/images/imagem-01.jpeg'
+            },
+            product2: {
+                title: 'Filme 02',
+                description: 'Filme muito legal (2)',
+                image: './assets/images/imagem-02.jpeg'
+            }
         }
     }
 }
